8dab8c745c4ed9a2d9f18cb294aa4046642ce9b9
Brasco <brasco@brascoder.com>
2015-03-17 22:23:11 -0400

    Add deck model

---
+++ app/controllers/decks_controller.rb
@@ -0,0 +1,40 @@
+class DecksController < ApplicationController
+  def index
+    @decks = Deck.all
+  end
+
+  def show
+    @deck = Deck.find(params[:id])
+  end
+
+  def new
+    @deck = Deck.new
+  end
+
+  def create
+    @deck = Deck.new(deck_params)
+    @deck.save
+    redirect_to decks_path
+  end
+
+  def edit
+    @deck = Deck.find(params[:id])
+  end
+
+  def update
+    @deck = Deck.find(params[:id])
+    @deck.update_attributes(deck_params)
+    redirect_to deck_path(@deck)
+  end
+
+  def destroy
+    Deck.delete(params[:id])
+    redirect_to decks_path
+  end
+
+  private
+
+  def deck_params
+    params.require(:deck).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get "/decks" => "decks#index"
+  get "/decks/new" => "decks#new"
+  get "/decks/:id" => "decks#show", as: :deck
+  post "/decks" => "decks#create"
+  get "/decks/:id/edit" => "decks#edit"
+  patch "/decks/:id" => "decks#update"
+  delete "decks/:id" => "decks#destroy"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
