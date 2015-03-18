09be28edd43d09ebc7e3dafca4644b4cd9794d18
Brasco <brasco@brascoder.com>
2015-03-18 06:09:45 -0400

    Add resource routes and replace static URL strings

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get "/decks" => "decks#index"
-  get "/decks/new" => "decks#new"
-  get "/decks/:id" => "decks#show", as: :deck
-  post "/decks" => "decks#create"
-  get "/decks/:id/edit" => "decks#edit"
-  patch "/decks/:id" => "decks#update"
-  delete "decks/:id" => "decks#destroy"
+  resources :decks
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
