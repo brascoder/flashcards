8e2f5f842a222e11bfc8cc34e1e8ea1ae6567b80
Brasco <brasco@brascoder.com>
2015-03-21 18:38:29 -0400

    Refactor deck controller and views

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -4,7 +4,7 @@ class DecksController < ApplicationController
   end
 
   def show
-    @deck = Deck.find(params[:id])
+    @deck = find_deck
     @cards = @deck.cards
   end
 
@@ -19,11 +19,11 @@ class DecksController < ApplicationController
   end
 
   def edit
-    @deck = Deck.find(params[:id])
+    @deck = 
   end
 
   def update
-    @deck = Deck.find(params[:id])
+    @deck = find_deck
     @deck.update_attributes(deck_params)
     redirect_to @deck
   end
@@ -37,5 +37,9 @@ class DecksController < ApplicationController
 
   def deck_params
     params.require(:deck).permit(:name)
+  end
+
+  def find_deck
+    Deck.find(params[:id])
   end
 end
