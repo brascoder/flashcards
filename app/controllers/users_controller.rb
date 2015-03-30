d18ae0eba523c8d39d9641821a4e5682df937745
Brasco <brasco@brascoder.com>
2015-03-29 22:20:35 -0400

    Validate user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,43 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+    if signed_in?
+      flash.alert = "You are signed in. Please sign out to create a new account."
+      redirect_to decks_path
+    else
+      @user = User.new
+    end
   end
 
   def create
     @user = User.new(user_params)
-    @user.password = encrypt(@user.password)
-    @user.save
-    sign_in @user
-    redirect_to decks_path
+    if @user.valid?
+      if signed_in?
+        flash.alert = "You are signed in. Please sign out to create a new account."
+      else
+        @user.password = encrypt(@user.password)
+        @user.save
+        sign_in @user
+      end
+      redirect_to decks_path
+    else
+      display_errors
+      redirect_to new_user_path
+    end
   end
 
   private
 
   def user_params
     params.require(:user).permit(:email, :password)
+  end
+
+  def display_errors
+    if @user.errors[:email]
+      flash.alert = "The email address #{@user.errors[:email][0]}."
+    elsif @user.errors[:password]
+      flash.alert = "Your password #{@user.errors[:password][0]}."
+    else
+      flash.alert = "Something is wrong with your info, please fix it."
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :email, uniqueness: true
+  validates :password, confirmation: true, length: { minimum: 6 }
   has_many :decks
 end
